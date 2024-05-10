19a260a9a141be229870d469f1a039c037a7bc65
Orbis25 <orbisalonzo@gmail.com>
2024-05-09 20:01:47 -0400

    feat: update to .net 8

--- FoundationKit.API.Example/Migrations/20240509235951_SolveNaming.Designer.cs
+++ FoundationKit.API.Example/Migrations/20240509235951_SolveNaming.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FoundationKit.API.Example.Domain.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FoundationKit.API.Example.Migrations
 {
     [DbContext(typeof(ApplicationIdentityDbContext))]
-    partial class ApplicationIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240509235951_SolveNaming")]
+    partial class SolveNaming
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
